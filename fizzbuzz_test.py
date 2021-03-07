9a98ab976e5ad435389edcd782404e60e29278da
Ziwen Tong <tongzi@oregonstate.edu>
2021-03-07 15:44:39 -0800

    wrote first test: test1. To test if the program output fizz when the input is 3

---
+++ fizzbuzz_test.py
@@ -0,0 +1,10 @@
+import unittest
+import fizzbuzz
+
+class TestCase(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(fizzbuzz.fb(3),"Fizz")
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
