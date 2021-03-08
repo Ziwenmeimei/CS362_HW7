993885a650273447517c1761aaa8d8a32fe5dd76
Ziwen Tong <tongzi@oregonstate.edu>
2021-03-07 16:03:38 -0800

    wrote second test: test2. To test if the input is 5 and the program return Buzz. Test2 failed.

--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -5,6 +5,9 @@ class TestCase(unittest.TestCase):
     def test1(self):
         self.assertEqual(fizzbuzz.fb(3),"Fizz")
 
+    def test2(self):
+        self.assertEqual(fizzbuzz.fb(5),"Buzz")
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
