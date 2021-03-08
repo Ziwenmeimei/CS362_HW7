ebdcc3696d41b23c5a988b6bebc22811c122fa1c
Ziwen Tong <tongzi@oregonstate.edu>
2021-03-07 17:10:22 -0800

    write third test: test3. To test if the input is 8 and the output is Leap year. Test3 failed

--- leapyear_test.py
+++ leapyear_test.py
@@ -8,6 +8,8 @@ class TestCase(unittest.TestCase):
     def test2(self):
         self.assertEqual(leapyear.isLeap(100),"Not leap year")
 
+    def test3(self):
+        self.assertEqual(leapyear.isLeap(8),"Leap year")
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
